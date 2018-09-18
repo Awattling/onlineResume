de8f70f00e097cd6f87d36face78c9925b2e5fbb
Awattling <awattling@shaw.ca>
2018-09-17 18:40:03 -0600

    React Router working

--- resume/src/Main.js
+++ resume/src/Main.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
-import {Home} from './components/Home';
+import {Experience} from './components/Experience';
 import {About} from './components/About';
-
+import {Skills} from './components/Skills';
+import {Projects} from './components/Projects';
+import {Contact} from './components/Contact';
 
 export class Main extends React.Component{
     render(){
         return(
             <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route path='/home' component={Home}/>
+                <Route exact path='/' component={About}/>
                 <Route path='/about' component={About}/>
+                <Route path='/skills' component={Skills}/>
+                <Route path='/experience' component={Experience}/>
+                <Route path='/projects' component={Projects}/>
+                <Route path='/contact' component={Contact}/>
             </Switch>
         );
     }
--- resume/src/components/Header.js
+++ resume/src/components/Header.js
@@ -1,11 +1,51 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
+import '../css/Header.css';
+import gear from '../media/orangeGear.png'
+import gitPic from '../media/gitMarkOrange.png';
 export class Header extends React.Component{
     render(){
-        return(
-            <header>
-              <h1>Austin Wattling</h1>
-            </header>
-        );
+        return <div>
+            <div className={"row"}>
+
+                <div className={"col-2 offset-2 center"}>
+                    <div className={"imgHelper"}></div>
+                    <img className={"spinGear"} src={gear} alt={""}/>
+                </div>
+                <div className={"col-4 center"}>
+                    <h1>Austin Wattling</h1>
+                </div>
+                <div className={"col-2 center"}>
+                    <div className={"imgHelper"}></div>
+                    <a href={"https://github.com/Awattling"}>
+                        <img className={"gitPic"} src={gitPic} alt={""} title={"Check out my Github"}/>
+                    </a>
+
+                </div>
+            </div>
+            <div className={"row"}>
+                <div className={"col-4 offset-4 center"}>
+                    <nav className={"navbar navbar-expand-sm bg-dark center"}>
+                        <ul className={"navbar-nav"}>
+                            <li className={"nav-item"}>
+                                <Link to={'/about'} className={"nav-link"}>About</Link>
+                            </li>
+                            <li className={"nav-item"}>
+                                <Link to={'/experience'} className={"nav-link"}>Experience</Link>
+                            </li>
+                            <li className={"nav-item"}>
+                                <Link to={'/skills'} className={"nav-link"}>Skills</Link>
+                            </li>
+                            <li className={"nav-item"}>
+                                <Link to={'/projects'} className={"nav-link"}>Projects</Link>
+                            </li>
+                            <li className={"nav-item"}>
+                                <Link to={'/contact'} className={"nav-link"}>Contact</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>;
     }
 }
