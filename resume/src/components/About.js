6c04c125e31fd7ecb51619d0748fba9f84f6247f
Awattling <awattling@shaw.ca>
2018-09-19 16:19:05 -0600

    Working on About page

--- resume/src/components/About.js
+++ resume/src/components/About.js
@@ -1,10 +1,65 @@
 import React from "react";
+import ReactNbsp from "react-nbsp";
+import styles from "../css/About.css";
+import screw from '../media/screw.png'
 
 export class About extends React.Component{
     render(){
         return(
-            <div>
-                <h1>About</h1>
+            <div className={styles.aboutMain}>
+                <div className={styles.stick}>
+                   <div className={styles.me}>
+                       I am a
+                   </div>
+                    <div className={styles.change}>
+                        Video Gamer
+                    </div>
+                    <div className={styles.line}>
+                    </div>
+                </div>
+                <div className={styles.videoGames}>
+                    <div className={styles.textLeft}>
+                        <p>
+                            <ReactNbsp/><ReactNbsp/><ReactNbsp/><ReactNbsp/><ReactNbsp/><ReactNbsp/><ReactNbsp/>
+                            Evening games become late night games which become early morning games. In my free time I face the never ending struggle to beat the next boss,
+                            build the best tower or explore the fantasy worlds. I discovered video games quite young and have not been able to leave their grasp. In fact I'm
+                            trying to make a life long career of the parts I love about it. The imagination of someone is not bound by the physical world and thus contains no
+                            limits. It's in this world of no limits that video games are born. On of my favorite games is Minecraft as it leaves you in a world seemingly without
+                            limits to create a world of your own. I am guilty of letting my imagination take hold of my time in Minecraft and as result I built some pretty cool
+                            things.
+                        </p>
+                    </div>
+                    <div className={styles.picRight}>
+                        <img src={screw} alt={""}/>
+                    </div>
+                </div>
+                <div className={styles.middle}>
+                        Musician
+                    <div className={styles.text}>
+                        <p>
+                            Evening games become late night games which become early morning games. In my free time I face the never ending struggle to beat the next boss,
+                            build the best tower or explore the fantasy worlds. I discovered video games quite young and have not been able to leave their grasp. In fact I'm
+                            trying to make a life long career of the parts I love about it. The imagination of someone is not bound by the physical world and thus contains no
+                            limits. It's in this world of no limits that video games are born. On of my favorite games is Minecraft as it leaves you in a world seemingly without
+                            limits to create a world of your own. I am guilty of letting my imagination take hold of my time in Minecraft and as result I built some pretty cool
+                            things.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.bottom}>
+                        Challenger
+                    <div className={styles.text}>
+                        <p>
+                            Evening games become late night games which become early morning games. In my free time I face the never ending struggle to beat the next boss,
+                            build the best tower or explore the fantasy worlds. I discovered video games quite young and have not been able to leave their grasp. In fact I'm
+                            trying to make a life long career of the parts I love about it. The imagination of someone is not bound by the physical world and thus contains no
+                            limits. It's in this world of no limits that video games are born. On of my favorite games is Minecraft as it leaves you in a world seemingly without
+                            limits to create a world of your own. I am guilty of letting my imagination take hold of my time in Minecraft and as result I built some pretty cool
+                            things.
+                        </p>
+                    </div>
+                </div>
             </div>
         );
     }
--- resume/src/components/Header.js
+++ resume/src/components/Header.js
@@ -4,22 +4,20 @@ import gear from '../media/orangeGear.png'
 import gitPic from '../media/gitMarkOrange.png';
 export class Header extends React.Component{
     render(){
-        return <div>
-            <div className={styles.row}>
-                <div className={styles.left}>
-                    <div className={styles.imgHelper}></div>
-                    <img className={styles.spinGear} src={gear} alt={""}/>
-                </div>
-                <div className={styles.middle}>
-                    <h1>Austin Wattling</h1>
-                </div>
-                <div className={styles.right}>
-                    <div className={styles.imgHelper}></div>
-                    <a href={"https://github.com/Awattling"}>
-                        <img className={styles.gitPic} src={gitPic} alt={""} title={"Check out my Github"}/>
-                    </a>
+        return <div className={styles.row}>
+            <div className={styles.left}>
+                <div className={styles.imgHelper}></div>
+                <img className={styles.spinGear} src={gear} alt={""}/>
+            </div>
+            <div className={styles.middle}>
+                <h1>Austin Wattling</h1>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.imgHelper}></div>
+                <a href={"https://github.com/Awattling"}>
+                    <img className={styles.gitPic} src={gitPic} alt={""} title={"Check out my Github"}/>
+                </a>
 
-                </div>
             </div>
         </div>;
     }
