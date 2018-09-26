df730aef6d58e03358ecf7dea4cd58006f9de70e
Awattling <awattling@shaw.ca>
2018-09-26 14:02:53 -0600

    Minor quality of life fixes

--- resume/src/components/Experience.js
+++ resume/src/components/Experience.js
@@ -13,58 +13,71 @@ export class Experience extends React.Component{
             company: "",
         }
     }
-    hoverOnCat(){
-        this.setState({
-            catHover: true,
-            company: "catering"
-        });
-    }
-    hoverOffCat(){
-        this.setState({
-            catHover: false,
-        });
-    }
-    hoverOnTicket(){
-        this.setState({
-            ticketHover: true,
-            company: "ticketmaster",
-        });
-    }
-    hoverOffTicket(){
-        this.setState({
-            ticketHover: false,
-        });
-    }
-    hoverOnBand(){
-        this.setState({
-            bandHover: true,
-            company: "marching",
-        });
-    }
-    hoverOffBand(){
-        this.setState({
-            bandHover: false,
-        });
-    }
-    hoverOnXa(){
-        this.setState({
-            xaHover: true,
-            company: "xa",
-        });
-    }
-    hoverOffXa(){
-        this.setState({
-            xaHover: false,
-        });
-    }
-    showModal(){
-        this.setState({
-            visible: true
-        });
-    }
+    hoverOn = (comp) => {
+        if(comp === "marching"){
+            this.setState({
+                bandHover: true,
+            });
+        }else if(comp === "cat"){
+            this.setState({
+                catHover: true,
+            });
+        }else if(comp === "xa"){
+            this.setState({
+                xaHover: true,
+            });
+        }else if(comp === "ticket"){
+            this.setState({
+                ticketHover: true,
+            })
+        }
+    };
+    hoverOff = (comp) => {
+        if(comp === "marching"){
+            this.setState({
+                bandHover: false,
+            });
+        }else if(comp === "cat"){
+            this.setState({
+                catHover: false,
+            });
+        }else if(comp === "xa"){
+            this.setState({
+                xaHover: false,
+            });
+        }else if(comp === "ticket"){
+            this.setState({
+                ticketHover: false,
+            })
+        }
+    };
+    clickOn = (comp) => {
+        if(comp === "marching"){
+            this.setState({
+                visible: true,
+                company: "marching",
+            });
+        }else if(comp === "cat"){
+            this.setState({
+                visible: true,
+                company: "catering",
+            });
+        }else if(comp === "xa"){
+            this.setState({
+                visible: true,
+                company: "xa",
+            });
+        }else if(comp === "ticket"){
+            this.setState({
+                visible: true,
+                company: "ticketmaster",
+            })
+        }
+    };
     hideModal(){
         this.setState({
-            visible: false
+            visible: false,
+
         });
     }
     render(){
@@ -73,59 +86,59 @@ export class Experience extends React.Component{
                 <div className={style.container}>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2018</h1>
+                            <h2>2018</h2>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2017</h1>
+                            <h2>2017</h2>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2016</h1>
+                            <h2>2016</h2>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2015</h1>
+                            <h2>2015</h2>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2014</h1>
+                            <h2>2014</h2>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.year}>
-                            <h1>2013 - 1995</h1>
+                            <h2>2013 - 1995</h2>
                         </div>
                     </div>
                 </div>
                 <div className={style.containerOverlay}>
                     <div className={style.row}>
-                        <div className={this.state.bandHover ? style.bandHover : style.band} onMouseEnter={this.hoverOnBand.bind(this)} onMouseLeave={this.hoverOffBand.bind(this)} onClick={this.showModal.bind(this)}/>
-                        <div className={this.state.ticketHover ? style.ticketmasterHover : style.ticketmaster} onMouseEnter={this.hoverOnTicket.bind(this)} onMouseLeave={this.hoverOffTicket.bind(this)} onClick={this.showModal.bind(this)}/>
-                        <div className={this.state.xaHover ? style.xaHover : style.xa}  onMouseEnter={this.hoverOnXa.bind(this)} onMouseLeave={this.hoverOffXa.bind(this)} onClick={this.showModal.bind(this)}/>
-                        <div className={this.state.catHover ? style.cateringAHover : style.cateringA} onMouseEnter={this.hoverOnCat.bind(this)} onMouseLeave={this.hoverOffCat.bind(this)} onClick={this.showModal.bind(this)}/>
-                        <div className={this.state.catHover ? style.cateringBHover : style.cateringB} onMouseEnter={this.hoverOnCat.bind(this)} onMouseLeave={this.hoverOffCat.bind(this)} onClick={this.showModal.bind(this)}/>
+                        <div className={this.state.bandHover ? style.bandHover : style.band} onMouseEnter={() => this.hoverOn("marching")} onMouseLeave={() => this.hoverOff("marching")} onClick={() => this.clickOn("marching")}/>
+                        <div className={this.state.ticketHover ? style.ticketmasterHover : style.ticketmaster} onMouseEnter={() => this.hoverOn("ticket")} onMouseLeave={() => this.hoverOff("ticket")} onClick={() => this.clickOn("ticket")}/>
+                        <div className={this.state.xaHover ? style.xaHover : style.xa}  onMouseEnter={() => this.hoverOn("xa")} onMouseLeave={() => this.hoverOff("xa")} onClick={() => this.clickOn("xa")}/>
+                        <div className={this.state.catHover ? style.cateringAHover : style.cateringA} onMouseEnter={() => this.hoverOn("cat")} onMouseLeave={() => this.hoverOff("cat")} onClick={() => this.clickOn("cat")}/>
+                        <div className={this.state.catHover ? style.cateringBHover : style.cateringB} onMouseEnter={() => this.hoverOn("cat")} onMouseLeave={() => this.hoverOff("cat")} onClick={() => this.clickOn("cat")}/>
                     </div>
                 </div>
                 <div className={style.containerOverlay}>
                     <div className={style.row}>
-                        <div className={style.bandText} onMouseEnter={this.hoverOnBand.bind(this)} onMouseLeave={this.hoverOffBand.bind(this)} onClick={this.showModal.bind(this)}>
+                        <div className={style.bandText} onMouseEnter={() => this.hoverOn("marching")} onMouseLeave={() => this.hoverOff("marching")} onClick={() => this.clickOn("marching")}>
                             Marching Band
                         </div>
-                        <div className={style.ticketText} onMouseEnter={this.hoverOnTicket.bind(this)} onMouseLeave={this.hoverOffTicket.bind(this)} onClick={this.showModal.bind(this)}>
+                        <div className={style.ticketText} onMouseEnter={() => this.hoverOn("ticket")} onMouseLeave={() => this.hoverOff("ticket")} onClick={() => this.clickOn("ticket")}>
                             Ticketmaster
                         </div>
-                        <div className={style.cateringAText} onMouseEnter={this.hoverOnCat.bind(this)} onMouseLeave={this.hoverOffCat.bind(this)} onClick={this.showModal.bind(this)}>
+                        <div className={style.cateringAText} onMouseEnter={() => this.hoverOn("cat")} onMouseLeave={() => this.hoverOff("cat")} onClick={() => this.clickOn("cat")}>
                             Stampede Catering
                         </div>
-                        <div className={style.cateringBText} onMouseEnter={this.hoverOnCat.bind(this)} onMouseLeave={this.hoverOffCat.bind(this)} onClick={this.showModal.bind(this)}>
+                        <div className={style.cateringBText} onMouseEnter={() => this.hoverOn("cat")} onMouseLeave={() => this.hoverOff("cat")} onClick={() => this.clickOn("cat")}>
                             Stampede Catering
                         </div>
-                        <div className={style.xaText} onMouseEnter={this.hoverOnXa.bind(this)} onMouseLeave={this.hoverOffXa.bind(this)} onClick={this.showModal.bind(this)}>
+                        <div className={style.xaText} onMouseEnter={() => this.hoverOn("xa")} onMouseLeave={() => this.hoverOff("xa")} onClick={() => this.clickOn("xa")}>
                             Xa. Security
                         </div>
                     </div>
