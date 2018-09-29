dca6658fabece8d805fb634dfce73d5cb8290413
Awattling <awattling@shaw.ca>
2018-09-29 17:23:42 -0600

    Contact changes

--- resume/src/Main.js
+++ resume/src/Main.js
@@ -5,18 +5,20 @@ import {About} from './components/About';
 import {Skills} from './components/Skills';
 import {Projects} from './components/Projects';
 import {Contact} from './components/Contact';
+import {Resume} from './components/Resume';
 
 export class Main extends React.Component{
     render(){
         return(
-            <Switch>
-                <Route exact path='/' component={About}/>
-                <Route path='/about' component={About}/>
-                <Route path='/skills' component={Skills}/>
-                <Route path='/experience' component={Experience}/>
-                <Route path='/projects' component={Projects}/>
-                <Route path='/contact' component={Contact}/>
-            </Switch>
+        <Switch location={window.location}>
+            <Route exact path='/' component={About}/>
+            <Route path='/about' component={About}/>
+            <Route path='/skills' component={Skills}/>
+            <Route path='/experience' component={Experience}/>
+            <Route path='/projects' component={Projects}/>
+            <Route path='/contact' component={Contact}/>
+            <Route path='/resume' component={Resume}/>
+        </Switch>
         );
     }
 }
--- resume/src/components/Contact.js
+++ resume/src/components/Contact.js
@@ -1,10 +1,25 @@
 import React from "react";
-import {Construction} from "./Construction";
+import style from "../css/Contact.css";
+import {NavLink} from "react-router-dom";
 export class Contact extends React.Component{
     render(){
         return(
             <div>
-                <Construction/>
+                <div className={style.wrapper}>
+                    <div className={style.needWork}>
+                        At this time I am currently looking for employment and would love to be contacted about any opportunities within the technology field which may be a fit.
+                    </div>
+                    <div className={style.if}>
+                        If you have such opportunity?
+                    </div>
+                    <div className={style.resume}>
+                        <NavLink to={'/resume'} className={"nav-link"}>See my Resume</NavLink>
+                        and contact me
+                    </div>
+                    <div className={style.git}>
+                        Also checkout my Github to see what I'm up to
+                    </div>
+                </div>
             </div>
         );
     }
