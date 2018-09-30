9292da3ac1be27956f2577cde6664432247a7ab8
Awattling <awattling@shaw.ca>
2018-09-30 16:03:48 -0600

    chrome bug fixed

--- resume/src/components/AboutMiddle2.js
+++ resume/src/components/AboutMiddle2.js
@@ -6,22 +6,26 @@ import family from '../media/family.jpg';
 export class AboutMiddle2 extends React.Component{
     render(){
         return(
-            <div>
                 <div className={styleLocal.middle}>
-                    <div className={styleLocal.musicText2}>
-                        It's safe to say that my friends and family make up the <font color="#ff8c00"> foundation </font> of who I am. I have been blessed with a wonderful
-                        family who have supported me through everything and will continue to do so. I have also been blessed with an incredible group
-                        of friends who are always willing to go on adventures.
-                    </div>
-                    <div className={styleLocal.middle}>
-                        <div className={styleLocal.m2Left}>
-                            <img className={styleMain.fill} src={family} alt={""}/>
-                        </div>
-                        <div className={styleLocal.m2Right}>
-                            <img className={styleMain.fill} src={friends} alt={""}/>
+                    <div className={styleLocal.container}>
+                        <div className={styleLocal.middle}>
+                            <div className={styleLocal.musicText2}>
+                                It's safe to say that my friends and family make up the <font color="#ff8c00"> foundation </font> of who I am. I have been blessed with a wonderful
+                                family who have supported me through everything and will continue to do so. I have also been blessed with an incredible group
+                                of friends who are always willing to go on adventures.
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>);
+                    <div className={styleLocal.container}>
+                        <div className={styleLocal.middle}>
+                            <div className={styleLocal.m2Left}>
+                                <img className={styleMain.fill} src={family} alt={""}/>
+                            </div>
+                            <div className={styleLocal.m2Right}>
+                                <img className={styleMain.fill} src={friends} alt={""}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>);
     }
 }
